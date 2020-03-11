85ddcd56c451503d82c3084655839ea7570dd38a
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2020-03-11 14:14:15 -0300

    [WIP] Bubble sort [Add] ProjectSettings, Packages
    >Script to get array values and organize them into a table structure.
    >Basic work in progress bubble sort algorithm script.

---
+++ Assets/Scripts/BubbleSort.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BubbleSort : MonoBehaviour
+{
+    public VectorValues vectorValues;
+    public void Sort()
+    {
+        StartCoroutine(DelayAndSort());
+    }
+
+
+
+    IEnumerator DelayAndSort()
+    {
+
+
+        int temp;
+        for (int j = 0; j < vectorValues.arr.Length - 1; j++)
+        {
+            for (int i = 0; i < vectorValues.arr.Length - 1; i++)
+            {
+                
+                if (vectorValues.arr[i] > vectorValues.arr[i + 1])
+                {
+                    yield return new WaitForSeconds(1f);
+                    temp = vectorValues.arr[i + 1];
+                    vectorValues.arr[i + 1] = vectorValues.arr[i];
+                    vectorValues.arr[i] = temp;
+                    vectorValues.numbers[i].GetComponent<TextMeshProUGUI>().text = vectorValues.arr[i].ToString();
+                    vectorValues.numbers[i+1].GetComponent<TextMeshProUGUI>().text = vectorValues.arr[i+1].ToString();
+                    vectorValues.values[i].GetComponent<Slider>().value = vectorValues.arr[i];
+                    vectorValues.values[i+1].GetComponent<Slider>().value = vectorValues.arr[i+1];
+                }
+
+            }
+        }
+
+
+
+        yield return new WaitForSeconds(0.5f);
+    }
+
+}
---
+++ Assets/Scripts/VectorValues.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VectorValues : MonoBehaviour
+{
+    #region Variables
+
+    private int amount = 5;
+
+    public GameObject value;
+    public List<GameObject> values;
+    public Transform valuesParent;
+    private Vector2 valueAnchor;
+
+    public GameObject number;
+    public List<GameObject> numbers;
+    public Transform numbersParent;
+    private Vector2 numberAnchor;
+
+    public GameObject marker;
+
+    public GameObject button;
+
+    public int[] arr = { 12, 17, 29, 4, 7 };
+
+    #endregion
+
+    #region Monobehaviour
+
+    void Awake()
+    {
+        CreateLists();
+        GetAnchors();
+    }
+
+    void Start()
+    {
+        StartCoroutine(DelayAndAssign());
+    }
+    #endregion
+
+    #region Private Methods
+
+    private void CreateLists()
+    {
+        values = new List<GameObject>();
+        numbers = new List<GameObject>();
+        values.Add(value);
+        numbers.Add(number);
+    }
+
+    private void GetAnchors()
+    {
+        valueAnchor = value.GetComponent<RectTransform>().anchoredPosition;
+        numberAnchor = number.GetComponent<RectTransform>().anchoredPosition;
+    }
+
+    #endregion
+
+    #region Coroutines and Protected Methods
+
+    private IEnumerator DelayAndAssign()
+    {
+
+        value.GetComponent<Slider>().value = arr[0];
+        number.GetComponent<TextMeshProUGUI>().text = arr[0].ToString();
+
+        for (int i = 1; i < amount; i++)
+        {
+            yield return new WaitForSeconds(0f);
+
+            values.Add(Instantiate(value, valuesParent, false));
+            valueAnchor.x = (valueAnchor.x + 50);
+            values[i].GetComponent<RectTransform>().anchoredPosition = valueAnchor;
+            values[i].GetComponent<Slider>().value = arr[i];
+
+            numbers.Add(Instantiate(number, numbersParent, false));
+            numberAnchor.x = (numberAnchor.x + 50);
+            numbers[i].GetComponent<RectTransform>().anchoredPosition = numberAnchor;
+            numbers[i].GetComponent<TextMeshProUGUI>().text = arr[i].ToString();
+        }
+    }
+
+    #endregion
+
+}
